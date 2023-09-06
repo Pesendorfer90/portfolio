49741e2e4b7dd0e52d3171bfd5e0b21834906caf
Pesendorfer90 <pesendorfer.gerald@hotmail.com>
2023-09-06 02:21:52 +0200

    add animation on navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,9 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  public menu: boolean = false;
+  public burgerIndex: number = 1;
+  public buttonDisabled: boolean = false;
 
   navMenu() {
-    console.log('burger klick');
-    
+    console.log(this.buttonDisabled);
+    this.buttonDisabled = true;
+    console.log(this.buttonDisabled);
+    this.menu = !this.menu;
+    this.animateBurger();
+    setTimeout(() => {
+      this.buttonDisabled = false;
+      console.log(this.buttonDisabled);
+    }, 10.5)
+  }
+
+  animateBurger() {
+    if (this.burgerIndex == 1) {
+      this.openBurger();
+    } else {
+      this.closeBurger();
+    }
+  }
+
+  openBurger() {
+    const interval = setInterval(() => {
+      this.burgerIndex < 5 ? this.burgerIndex++ : (clearInterval(interval));
+    }, 56);
+  }
+
+  closeBurger() {
+    const interval = setInterval(() => {
+      this.burgerIndex > 1 ? this.burgerIndex-- : (clearInterval(interval));
+    }, 56);
   }
 }
