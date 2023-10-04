16582568ddd3c22fe763ebc277a1b6b9af84660c
Pesendorfer90-Mobil <pesendorfer.gerald@hotmail.com>
2023-10-04 21:38:20 +0200

    add scss to portfolio.component and add a JSON string for description infos

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -9,23 +9,33 @@ export class PortfolioComponent {
   items = [
     {
       image: './assets/img/portfolio-img/epl.png',
-      text: 'Text 1'
+      name: 'Pollo Loco',
+      techStack: 'JavaScript | HTML | CSS',
+      description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.'
     },
     {
       image: './assets/img/portfolio-img/join.PNG',
-      text: 'Text 2'
+      name: 'Join',
+      techStack: 'Angular | TypeScript | HTML | CSS | Firebase',
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. '
     },
     {
       image: './assets/img/portfolio-img/pokedex.PNG',
-      text: 'Text 3'
+      name: 'Pokédex',
+      techStack: 'JavaScript | HTML | CSS | Api',
+      description: 'Based on the PokéAPI a simple library that provides and catalogues pokemon information.'
     },
     {
       image: './assets/img/portfolio-img/under-construction.jpg',
-      text: 'Text 4'
+      name: '',
+      techStack: '',
+      description: ''
     },
     {
       image: './assets/img/portfolio-img/under-construction.jpg',
-      text: 'Text 5'
+      name: '',
+      techStack: '',
+      description: ''
     }
   ];
 }
