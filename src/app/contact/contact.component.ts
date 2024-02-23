5f9530586eadfe6bac75d33e65d25ac2b43a6a5f
Pesendorfer90-Mobil <pesendorfer.gerald@hotmail.com>
2024-02-23 21:10:00 +0100

    update validation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
+import { ContactForm } from '../contact-form';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
+
 export class ContactComponent {
   isHovered: boolean = false;
   nameFocus: boolean = false;
   emailFocus: boolean = false;
   messageFocus: boolean = false;
+
+  messageData = new ContactForm('ABC', 'DEF', 'GHI');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  // setValue() {
+  //   this.contact.name = '';
+  // }
+
+
+//  zum testen 
+//   const myContact = new ContactForm
+//   ('SkyDog', 'Fetch any object at any distance', 'Leslie Rollover');
+
+// console.log('My CONTACT is called ' + myContact.name);
+
+
+
+
+// Letzter ChatGPT eintrag BEACHTEN dort steht die LÖSUNG
 }
