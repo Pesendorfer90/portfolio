5c44f7f238585f793685e48a441a3a88c00c93ec
Pesendorfer90-Mobil <pesendorfer.gerald@hotmail.com>
2024-04-02 21:26:12 +0200

    change Formcontroll on contact component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+// import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -13,35 +15,70 @@ export class ContactComponent {
   emailFocus: boolean = false;
   messageFocus: boolean = false;
 
-  meinFormular: FormGroup;
-  name: FormControl;
-  email: FormControl;
-  message: FormControl;
-  checkBox: FormControl;
 
-  constructor() {
-    this.name = new FormControl('', [Validators.required, Validators.minLength(5)]);
-    this.email = new FormControl('', [Validators.required, Validators.email]);
-    this.message = new FormControl('', [Validators.required, Validators.minLength(10)]);
-    this.checkBox = new FormControl('', Validators.required);
 
-    this.meinFormular = new FormGroup({
-      name: this.name,
-      email: this.email,
-      message: this.message,
-      checkBox: this.checkBox
-    });
-    console.log(this.meinFormular.value);
+
+
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+    privatPolicy: false,
   }
 
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted) {
+      console.log(this.contactData);
+      
+    }
+  }
+
+
+
+
+
+
+
+  // meinFormular: FormGroup;
+  // name: FormControl;
+  // email: FormControl;
+  // message: FormControl;
+  // checkBox: FormControl;
+
+  // constructor() {
+  //   this.name = new FormControl('', [Validators.required, Validators.minLength(5)]);
+  //   this.email = new FormControl('', [Validators.required, Validators.email]);
+  //   this.message = new FormControl('', [Validators.required, Validators.minLength(10)]);
+  //   this.checkBox = new FormControl('', Validators.required);
+
+  //   this.meinFormular = new FormGroup({
+  //     name: this.name,
+  //     email: this.email,
+  //     message: this.message,
+  //     checkBox: this.checkBox
+  //   });
+  //   console.log(this.meinFormular.value);
+  // }
+
+  // logFormValue() {
+  //   console.log(this.meinFormular.value);
+  // }
+
   logFormValue() {
-    console.log(this.meinFormular.value);
+    console.log(this.contactData);
   }
 
   onCheckboxChange() {
-    if (!this.meinFormular.get('checkBox').value) {
-      this.meinFormular.get('checkBox').setValue('');
-      console.log(this.meinFormular.get('checkBox').value);
+    if (this.contactData.privatPolicy) {
+      // this.meinFormular.get('checkBox').setValue('');
+      // console.log(this.meinFormular.get('checkBox').value);
     }    
   }
+
+  // onCheckboxChange() {
+  //   if (!this.meinFormular.get('checkBox').value) {
+  //     this.meinFormular.get('checkBox').setValue('');
+  //     console.log(this.meinFormular.get('checkBox').value);
+  //   }    
+  // }
 }
