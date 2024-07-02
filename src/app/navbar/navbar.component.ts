2de0873a7018158b5df3d50327f5f79a8260b147
Pesendorfer90-Mobil <pesendorfer.gerald@hotmail.com>
2024-07-02 18:49:06 +0200

    mobile responsiv navbar update 2

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit, Renderer2 } from '@angular/core';
 import { scrollToElement } from '../functions';
 
 @Component({
@@ -6,16 +6,65 @@ import { scrollToElement } from '../functions';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   menu: boolean = false;
   burgerIndex: number = 1;
   isAnimating: boolean = false;
-  linkId: string[]  = ['aboutMeLink', 'mySkillsLink', 'portfolioLink'];
+  // linkId: string[]  = ['aboutMeLink', 'mySkillsLink', 'portfolioLink'];
+  
+  
+  
+  
+  linkId  = [
+    {
+      "id": "'aboutMeLink', 'mySkillsLink', 'portfolioLink'",
+      "size": "170, 580, 96"
+    },
+  ]
+  
+
+
+
+
   linkAnimationStates: { [key: string]: { enter: boolean, leave: boolean, down: boolean } } = {};
 
-  constructor() {
+  constructor(private renderer: Renderer2) {
     (window as any).myComponentInstance = this;
   }
+
+
+
+
+
+  ngOnInit() {
+    this.setBoarderContainerHeight();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.setBoarderContainerHeight();
+  }
+
+  private setBoarderContainerHeight() {
+    const container = ('')
+    const viewportHeight = window.innerHeight;
+    const heightInVh = 64;
+    const resultInPx = 170 / (viewportHeight * (heightInVh / 100) / 100);
+
+
+    this.linkId.forEach(linkId => this.onMouseOut(linkId));
+
+    container = document.getElementById(linkId);
+    if (container) {
+      this.renderer.setStyle(container, '--boarder-container-height', `${resultInPx}px`);
+    }
+  }
+
+
+
+
+
+
 
   scrollToArea(link: string) {
     this.navMenu();
